b7658ff08459a388cfece48462e84287541f6c84
Youness <youness.assassi@gmail.com>
2015-04-21 21:16:28 -0400

    use grunt to structure the project files for distribution

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+module.exports = function(grunt){
+	require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        htmlhint: {
+		    build: {
+		        options: {
+		            'tagname-lowercase': true,
+		            'attr-lowercase': true,
+		            'attr-value-double-quotes': true,
+		            'doctype-first': true,
+		            'spec-char-escape': true,
+		            'id-unique': true
+		        },
+		        src: ['src/index.html']
+		    }
+		},
+        // Keep JavaScript as lean as possible
+		uglify: {
+		    build: {
+		    	files: {
+		    		'dist/js/lib/bootstrap.min.js': ['src/js/lib/bootstrap.js'],
+		    		'dist/js/lib/jquery-1.11.2.min.js': ['src/js/lib/jquery-1.11.2.min.js'],
+		    		'dist/js/lib/knockout-3.2.0.min.js': ['src/js/lib/knockout-3.2.0.js']
+		    	}
+		    }
+		},
+		// keep CSS as lean as possible
+		cssmin: {
+		  target: {
+		    files: {
+		      'dist/css/lib/bootstrap.min.css': ['src/css/lib/bootstrap.css'],
+		      'dist/css/lib//bootstrap-theme.min.css': ['src/css/lib/bootstrap-theme.css']
+		    }
+		  }
+		},
+		//inline css and js within html files
+		inline: {
+			dist: {
+				options: {
+					cssmin: true,
+					uglify: true
+				},
+				files: {
+					'dist/index.html': 'src/index.html',
+				}
+			}
+	    },
+  		// Run automated tasks when files are updated
+  		watch: {
+		    html: {
+		        files: ['src/index.html'],
+		        tasks: ['htmlhint', 'inline']
+		    }
+		}
+    });
+
+	// Default task.
+  	grunt.registerTask('default', ['htmlhint', 'cssmin', 'uglify', 'inline', 'watch']);
+};
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Neighborhood map project by Youness Assassi-  Washington DC Notable locations
 
-The resulting website can be accessed <a href="http://younessassassi.github.io/frontend-nanodegree-neighborhood-map/#">here</a>
+The resulting website can be accessed <a href="http://younessassassi.github.io/frontend-nanodegree-neighborhood-map/dist/#">here</a>
 
-Note: assets folder contains all source files before css and js minification and inlining.
+Note: src folder contains all source files before css and js minification and inlining while the dist folder contains the distribution files.
 
 This project utilizes Knockoutjs library, the Model-View-View Model (MVVM) pattern, Jquery and Bootstrap.  It also utilizes a few APIs, such as Google Maps, New York Times and Wikepedia to provide spatial location along with related information and news.
 
